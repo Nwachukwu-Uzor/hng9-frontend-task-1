c43595475c7f92b30c10a0bc8cdb4ca142482d29
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-10-27 21:48:17 +0100

    fix: add ids to specified html elements on the header section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,10 +10,23 @@ const Header = () => {
   return (
     <Container className="flex items-center justify-center relative mt-4">
       <div className="flex flex-col items-center justify-center gap-1">
-        <img src={avatar} alt="" className="w-[100px] h-[100px] rounded-full" />
-        <h1 className="font-bold text-lg lg:text-[20px]">Uzor Nwachukwu</h1>
-        <img src={optionsBtn} className="absolute top-0 right-0 lg:hidden"/>
-        <img src={shareBtn} className="absolute top-0 right-[20%] hidden lg:block"/>
+        <img
+          src={avatar}
+          alt=""
+          className="w-[100px] h-[100px] rounded-full"
+          id="profile__img"
+        />
+        <h1 className="font-bold text-lg lg:text-[20px]" id="twitter">
+          Uzor_Nwachukwu
+        </h1>
+        <h1 className="hidden" id="slack">
+          uzor nwachukwu
+        </h1>
+        <img src={optionsBtn} className="absolute top-0 right-0 lg:hidden" />
+        <img
+          src={shareBtn}
+          className="absolute top-0 right-[20%] hidden lg:block"
+        />
       </div>
     </Container>
   );
