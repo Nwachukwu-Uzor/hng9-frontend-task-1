16f86c83266623764cb9a1daaf413cef9322f481
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-10-27 21:09:32 +0100

    add: header component to hold avatar, name and buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import Container from "./Container";
 
 import avatar from "../assets/images/avatar.jpg";
+import optionsBtn from "../assets/images/optionsBtn.svg";
+import shareBtn from "../assets/images/shareBtn.svg";
 
 const Header = () => {
   return (
-    <Container className="flex items-center justify-center relative">
-      <div>
-        <img src={avatar} alt="" />
+    <Container className="flex items-center justify-center relative mt-4">
+      <div className="flex flex-col items-center justify-center gap-1">
+        <img src={avatar} alt="" className="w-[100px] h-[100px] rounded-full" />
+        <h1 className="font-bold text-lg lg:text-[20px]">Uzor Nwachukwu</h1>
+        <img src={optionsBtn} className="absolute top-0 right-0 lg:hidden"/>
+        <img src={shareBtn} className="absolute top-0 right-[20%] hidden lg:block"/>
       </div>
     </Container>
   );
