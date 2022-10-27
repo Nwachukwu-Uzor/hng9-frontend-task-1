7b7d3ad7063c385c115304d0177bbb4b65fbef5f
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-10-27 22:04:14 +0100

    fix: explicit return type for links component

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "./Container";
 import LinkButton from "./LinkButton";
 
-const Links = () => {
+const Links = (): JSX.Element => {
   return (
     <Container className="flex flex-col gap-4">
       <LinkButton id="btn__zuri" displayText="Twitter Link" to="" />
