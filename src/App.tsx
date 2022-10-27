57cb157e5653db7cf69e1dd8cf18a893ef7b4947
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-10-27 21:47:23 +0100

    add: import for reusable link into the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Container from "./components/Container";
 import Header from "./components/Header";
+import LinkButton from "./components/LinkButton";
+import Links from "./components/Links";
 
 const App = () => {
   return (
     <main>
       <Header />
+      <Links />
     </main>
   );
 };
