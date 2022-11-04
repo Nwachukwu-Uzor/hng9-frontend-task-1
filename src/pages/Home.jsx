a5dd37d6baef9dab570a50eb2d1eaf83ab375b61
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-11-04 12:12:57 +0100

    add: pages folder to hold all pages in the application

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Links from "../components/Links";
+import Logos from "../components/Logos";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Links />
+      <Logos />
+    </>
+  );
+};
+
+export default Home;
